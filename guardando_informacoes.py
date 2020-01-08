6f6c4d8a43643c24671920117a65df23e14953b7
Guilherme <galanti_imp@hotmail.com>
2020-01-08 05:00:49 -0300

    Criação de arquivos csv e comentários no código

--- guardando_informacoes.py
+++ guardando_informacoes.py
@@ -1,6 +1,6 @@
 from listas_especificas import somente_nome, somente_nascimento, somente_formacao
 from escreve_arquivos import escrever_arquivo_csv
-from plotando_gráficos import plotar
+from plotando_graficos import plotar
 
 lista_nomes = somente_nome('usuario_nome_nascimento_formacao.csv')
 lista_nascimento = somente_nascimento('usuario_nome_nascimento_formacao.csv')
@@ -19,7 +19,7 @@ for i in range(0, tamanho_lista_nomes):
     contador_nomes = 0
     contador_datas = 0
     contador_formacao = 0
-
+    # Varrendo o banco de dados
     if lista_nomes[i] not in nomes:
         for j in range(0, tamanho_lista_nomes):
             if lista_nomes[i] == lista_nomes[j]:
@@ -32,7 +32,7 @@ for i in range(0, tamanho_lista_nomes):
         for j in range(0, tamanho_lista_nomes):
             if lista_formacao[i] == lista_formacao[j]:
                 contador_formacao += 1
-
+    # Contando a frequencia de nomes, ano de nascimento e formacao
     if contador_nomes > 1:
         nomes.append(lista_nomes[i])
         contagem_nomes.append(contador_nomes)
@@ -49,6 +49,7 @@ escrever_arquivo_csv('frequencia_nomes.csv',nomes,contagem_nomes)
 escrever_arquivo_csv('frequencia_datas.csv',datas,contagem_datas)
 escrever_arquivo_csv('frequencia_formacao.csv',formacao,contagem_formacao)
 
-plotar('Distribuição de nomes','Nomes', 'Frequência',nomes,contagem_nomes)
-plotar('Distribuição de ano de nascimento','Ano de nascimento', 'Frequência',datas,contagem_datas)
-plotar('Distribuição de formacao','Formação', 'Frequência',formacao,contagem_datas)
+# todo: Os gráficos estão péssimos, arrumar formatação
+#plotar('Distribuição de nomes','Nomes', 'Frequência',nomes,contagem_nomes)
+#plotar('Distribuição de ano de nascimento','Ano de nascimento', 'Frequência',datas,contagem_datas)
+#plotar('Distribuição de formacao','Formação', 'Frequência',formacao,contagem_datas)
